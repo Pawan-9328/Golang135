cb74601f96f9b0829b825a0ae08232af4ef871dc
Pawan Gola <pawangola908@gmail.com>
2024-06-15 18:13:45 +0530

    how to work package and variable in go lang

---
+++ doutil/doutil.go
@@ -0,0 +1,10 @@
+package doutil
+
+import (
+	//"fmt"
+
+)
+
+// func PrintMessage(message string){
+// 	fmt.Println(message)
+// }
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	//"135go/doutil"
+	"fmt"
+)
+
+func main() {
+	//  fmt.Println("learn go language ")
+	// //  fmt.Println("------------")
+
+	// //  doutil.PrintMessage("Hello World ")
+
+	// var name  string = "Keshav"; 
+	// var version  = "1.0"; 
+	// fmt.Println(name)
+	
+	// fmt.Println(version)
+
+	// var money int = 3700
+	// var currency = 3456
+	// fmt.Println(money)
+	// fmt.Println("currency: ", currency)
+
+	// var dimension float64 = 89.14
+	// fmt.Println(dimension) 
+
+   // var decided bool = false 
+	// decided = true
+	// fmt.Println(decided)
+
+	// var person = 43 
+	// fmt.Println(person)
+
+	// const pi = 67.43
+	// fmt.Println(pi)
+
+	// person := "Keshav Name"
+	// fmt.Println(person)  
+
+	// var Public = "date is imported"
+	// var private = "data is private"
+
+	// fmt.Println(Public)
+	// fmt.Println(private)
+
+	//.........Println / Printf .................
+
+	 age:=23
+	 name:="Alice"
+	 height := 5.667
+
+	 fmt.Println("age: ", age, "height: ", height, "name: ", name )
+	 fmt.Println("Hello World! ")
+
+	 fmt.Printf("age is %d\n ", age)
+	 fmt.Printf("height is %.2f\n", height)
+	 fmt.Printf("Type of name is %T\n",age)
+
+	 fmt.Printf("name is %s\n",name)
+
+
+
+}
+
+
+
+
+
+
+
+
